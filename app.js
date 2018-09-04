188a3f78cb0440798b88653ca1ef07bd58a711b4
Nolan Kovacik <noltron000@gmail.com>
2018-09-04 13:46:57 -0700

    connected to server, updated readme

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Review = mongoose.model('Review', {
+	title: String
+});
 
-var exphbs = require('express-handlebars');
+let exphbs = require('express-handlebars');
 
-let reviews = [
-	{ title: "Great Review" },
-	{ title: "Next Review" },
-	{ title: "Next Review" }
-]
+// let reviews = [
+// 	{ title: "Great Review" },
+// 	{ title: "Next Review" },
+// 	{ title: "Next Review" }
+// ];
+
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
 app.set('view engine', 'handlebars');
 
 app.listen(3000, () => {
 	console.log('App listening on port 3000!')
-})
+});
 
 app.get('/', (req, res) => {
-	res.render('reviews-index', { reviews: reviews });
-})
+	Review.find()
+		.then(reviews => {
+			res.render('reviews-index', { reviews: reviews })
+		})
+		.catch(err => {
+			console.log(err)
+		})
+});
 
 app.get('/reviews', (req, res) => {
-	res.render('reviews-index', { reviews: reviews });
-})
+	res.render('reviews-index', { reviews: reviews })
+});

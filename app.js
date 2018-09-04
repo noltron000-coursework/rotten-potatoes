f3e3a6a401e23ff6d14746ffedba90fed16efb7b
Nolan Kovacik <noltron000@gmail.com>
2018-09-04 12:06:15 -0700

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
-
-// app.js
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// app.js
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
 
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Hello World!' });

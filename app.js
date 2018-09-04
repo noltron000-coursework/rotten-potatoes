8837b74cf205202c38bee7fb6d11ae7f65ee0560
Nolan Kovacik <noltron000@gmail.com>
2018-09-04 14:54:26 -0700

    now you can create reviews

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
 const Review = mongoose.model('Review', {
-	title: String
+	title: String,
+	description: String,
+	movieTitle: String,
+	movieRating: String
 });
 
 let exphbs = require('express-handlebars');
@@ -14,6 +19,8 @@ let exphbs = require('express-handlebars');
 // ];
 
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
@@ -35,4 +42,18 @@ app.get('/', (req, res) => {
 
 app.get('/reviews', (req, res) => {
 	res.render('reviews-index', { reviews: reviews })
+});
+
+app.get('/reviews/new', (req, res) => {
+	res.render('reviews-new', {});
+});
+
+
+app.post('/reviews', (req, res) => {
+	Review.create(req.body).then((review) => {
+		console.log(review);
+		res.redirect('/');
+	}).catch((err) => {
+		console.log(err.message);
+	})
 });

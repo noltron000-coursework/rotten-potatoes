18abd64334211fa0cf4b52e1ea473d9b05bd5f5c
Nolan Kovacik <noltron000@gmail.com>
2018-09-06 14:48:16 -0700

    users can now see reviews

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // app.js
 var exphbs = require('express-handlebars');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+// let reviews = [
+// 	{ title: "Great Review" },
+// 	{ title: "Next Review" }
+// ]
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
 app.set('view engine', 'handlebars');
 
+// INDEX
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.render('reviews-index', { reviews: reviews })
 });
 
+// LISTEN
 app.listen(3000, () => {
 	console.log('App listening on port 3000!')
 });
-
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})

63d7c5b2a5712caff4fd1866e6383518d0446bc1
Nolan Kovacik <noltron000@gmail.com>
2018-09-06 14:56:39 -0700

    added mongoDB, using ODM, and whatnot

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 // app.js
-var exphbs = require('express-handlebars');
+
+
+//
+const mongoose = require('mongoose');
+const Review = mongoose.model('Review', {title: String});
 const express = require('express');
 const app = express();
+let exphbs = require('express-handlebars');
 
-// let reviews = [
-// 	{ title: "Great Review" },
-// 	{ title: "Next Review" }
-// ]
-
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-
 app.set('view engine', 'handlebars');
 
 // INDEX
 app.get('/', (req, res) => {
-	res.render('reviews-index', { reviews: reviews })
-});
+	Review.find()
+		.then(reviews => {
+			res.render('reviews-index', { reviews: reviews });
+		})
+		.catch(err => {
+			console.log(err);
+		})
+})
 
 // LISTEN
 app.listen(3000, () => {

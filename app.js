cf6676be0d65716cadd758716b054b66fa1754a5
Nolan Kovacik <noltron000@gmail.com>
2018-09-07 13:51:42 -0700

    Now users can create and view submissions.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// INDEX
+// INDEX => SHOW ALL REVIEW
 app.get('/', (req, res) => {
 	Review.find()
 	.then(reviews => {
@@ -36,23 +36,29 @@ app.get('/', (req, res) => {
 	})
 })
 
-
-// NEW REDIRECT THINGY. NOT REALLY CREATE
+// NEW => SHOW REVIEW CREATION FORM
 app.get('/reviews/new', (req, res) => {
 	res.render('reviews-new', {});
 })
 
 
-// CREATE NEW
-app.post('/reviews', (req, res) => {
-	Review.create(req.body).then((review) => {
-		console.log(review);
-		res.redirect('/');
+// SHOW SINGLE REVIEW
+app.get('/reviews/:id', (req, res) => {
+	Review.findById(req.params.id).then((review) => {
+		res.render('reviews-show', { review: review })
 	}).catch((err) => {
 		console.log(err.message);
 	})
 })
-
+// CREATE NEW REVIEW
+app.post('/reviews', (req, res) => {
+	Review.create(req.body).then((review) => {
+		console.log(review)
+		res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+	}).catch((err) => {
+		console.log(err.message)
+	})
+})
 
 // LISTEN
 app.listen(3000, () => {

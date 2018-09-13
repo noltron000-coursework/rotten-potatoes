db8b2db477f3f4f5dd7721603fb779f6a2a136f7
Nolan Kovacik <noltron000@gmail.com>
2018-09-13 11:16:02 -0700

    Dacio helped me fix Mocha and Chai. Thanks Dacio :)

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,7 +1,7 @@
 //
 // https://github.com/MakeSchool-Tutorials/Node-Rotten-Potatoes/issues/3
 //
-// const Review = require('./models/review');
+const Review = require('../models/review');
 //
 // export default function (app) {
 // 	app.get('/', (req, res) => {
@@ -15,16 +15,68 @@
 // 	})
 // }
 
-module.exports = function (app, Review) {
-
+function reviews (app) {
+	// INDEX => SHOW ALL REVIEW
 	app.get('/', (req, res) => {
 		Review.find()
-			.then(reviews => {
-				res.render('reviews-index', {reviews: reviews});
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	});
+		.then(reviews => {
+			res.render('reviews-index', { reviews: reviews });
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	})
 
+	// NEW => SHOW REVIEW CREATION FORM
+	app.get('/reviews/new', (req, res) => {
+		res.render('reviews-new', {});
+	})
+
+
+	// SHOW SINGLE REVIEW
+	app.get('/reviews/:id', (req, res) => {
+		Review.findById(req.params.id).then((review) => {
+			res.render('reviews-show', { review: review })
+		}).catch((err) => {
+			console.log(err.message);
+		})
+	})
+
+	// UPDATE SINGLE REVIEW
+	app.put('/reviews/:id', (req, res) => {
+		Review.findByIdAndUpdate(req.params.id, req.body).then(review => {
+			res.redirect(`/reviews/${review._id}`)
+		}).catch(err => {
+			console.log(err.message)
+		})
+	})
+
+	// EDIT => SHOW EDIT FORM FOR SINGLE REVIEW
+	app.get('/reviews/:id/edit', function (req, res) {
+		Review.findById(req.params.id, function(err, review) {
+			res.render('reviews-edit', {review: review});
+		})
+	})
+
+	// CREATE NEW REVIEW
+	app.post('/reviews', (req, res) => {
+		Review.create(req.body).then((review) => {
+			console.log(review)
+			res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+		}).catch((err) => {
+			console.log(err.message)
+		})
+	})
+
+	// DELETE SINGLE REVIEW
+	app.delete('/reviews/:id', function (req, res) {
+		console.log("DELETE review")
+		Review.findByIdAndRemove(req.params.id).then((review) => {
+			res.redirect('/');
+		}).catch((err) => {
+			console.log(err.message);
+		})
+	})
 }
+
+module.exports = reviews

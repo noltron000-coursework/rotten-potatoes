4340963419b9714b328d47389791f65191134fbb
Nolan Kovacik <noltron000@gmail.com>
2018-09-13 12:37:39 -0700

    now users have a comment form section

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,19 +1,5 @@
-//
-// https://github.com/MakeSchool-Tutorials/Node-Rotten-Potatoes/issues/3
-//
+
 const Review = require('../models/review');
-//
-// export default function (app) {
-// 	app.get('/', (req, res) => {
-// 		Review.find().then(reviews => {
-// 				res.render('reviews-index', {
-// 					reviews: reviews
-// 				})
-// 			}).catch(err => {
-// 				console.log(err)
-// 			})
-// 	})
-// }
 
 function reviews (app) {
 	// INDEX => SHOW ALL REVIEW

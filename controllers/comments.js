71a4ec16d460529685d7c0783f374fce4a67bf9d
Nolan Kovacik <noltron000@gmail.com>
2018-09-14 10:22:40 -0700

    PROJECT PART 1 COMPLETE

--- controllers/comments.js
+++ controllers/comments.js
@@ -17,4 +17,14 @@ module.exports = function(app) {
 		console.log('test2');
 		res.send('reviews comment');
 	});
+
+	// DELETE
+	app.delete('/reviews/comments/:id', function (req, res) {
+		console.log("DELETE comment")
+		Comment.findByIdAndRemove(req.params.id).then((comment) => {
+			res.redirect(`/reviews/${comment.reviewId}`);
+		}).catch((err) => {
+			console.log(err.message);
+		});
+	});
 };

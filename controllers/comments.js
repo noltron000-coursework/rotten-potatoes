85c00594cdf58fbdde57f2a934a3f75fdd908d2f
Nolan Kovacik <noltron000@gmail.com>
2018-09-16 15:23:07 -0700

    adding heroku things

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
 
 	// CREATE Comment
 	app.post('/reviews/comments', (req, res) => {
-		console.log('test1');
-		Comment.create(req.body).then(comment => {
+		Comment.create(req.body)
+		.then(comment => {
 			res.redirect(`/reviews/${comment.reviewId}`);
 		}).catch((err) => {
 			console.log(err.message);
@@ -14,14 +14,13 @@ module.exports = function(app) {
 
 	// NEW Comment
 	app.post('/reviews/comments/', (req, res) => {
-		console.log('test2');
 		res.send('reviews comment');
 	});
 
 	// DELETE
 	app.delete('/reviews/comments/:id', function (req, res) {
-		console.log("DELETE comment")
-		Comment.findByIdAndRemove(req.params.id).then((comment) => {
+		Comment.findByIdAndRemove(req.params.id)
+		.then((comment) => {
 			res.redirect(`/reviews/${comment.reviewId}`);
 		}).catch((err) => {
 			console.log(err.message);

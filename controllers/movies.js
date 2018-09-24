63ff9b482c84656b4011f1068444d5a2a1e579d5
Nolan Kovacik <noltron000@gmail.com>
2018-09-24 15:28:25 -0700

    needing some help in a tough spot

--- controllers/movies.js
+++ controllers/movies.js
@@ -17,33 +17,34 @@ function movies (app) {
 
 
 	app.get('/movies/:id', (req, res) => {
-		moviedb.movieInfo({id: req.params.id})
+		moviedb.movieInfo({ id: req.params.id })
 		.then(movie => {
 			res.render('movies-show', {
 				movie: movie
 			});
 		}).catch((err) => {
-			console.log(err.message);
+			console.log("ERROR: " + err.message);
 		});
 	});
 
 
-	// app.get('/movies/:id', (req, res) => {
-	// 	moviedb.movieInfo({ id: req.params.id })
-	// 	.then(movie => {
-	// 		if (movie.video) {
-	// 			moviedb.movieVideos({ id: req.params.id }).then(videos => {
-	// 				movie.trailer_youtube_id = videos.results[0].key
-	// 				renderTemplate(movie)
-	// 			})
-	// 		} else {
-	// 			renderTemplate(movie)
-	// 		}
-	// 		function renderTemplate(movie) {
-	// 			res.render('movies-show', { movie: movie });
-	// 		}
-	// 	}).catch(console.error)
-	// })
+	app.get('/movies/:id', (req, res) => {
+		moviedb.movieInfo({ id: req.params.id })
+		.then(movie => {
+			if (movie.video) {
+				moviedb.movieVideos({ id: req.params.id })
+				.then(videos => {
+					movie.trailer_youtube_id = videos.results[0].key
+					renderTemplate(movie)
+				})
+			} else {
+				renderTemplate(movie)
+			}
+			function renderTemplate(movie) {
+				res.render('movies-show', { movie: movie });
+			}
+		}).catch(console.error)
+	})
 
 	// // SHOW SINGLE REVIEW
 	// app.get('/reviews/:id', (req, res) => {

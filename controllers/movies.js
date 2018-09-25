225eb75988c78c9eb188cfe3f9564acc9972596b
Nolan Kovacik <noltron000@gmail.com>
2018-09-25 15:08:32 -0700

    now it works?

--- controllers/movies.js
+++ controllers/movies.js
@@ -20,21 +20,20 @@ function movies (app) {
 	// NEW => SHOW MOVIE CREATION FORM
 	// There is no need to create new movies!
 
+	// SHOW ROUTE? ADDING BEFORE SHOW SINGLE. IT WORKS.
+	app.get('/movies/:id', (req, res) => {
+		moviedb.movieInfo({ id: req.params.id }).then(movie => {
+			Review.find({ movieId: req.params.id }).then(reviews => {
+				res.render('movies-show', { movie: movie, reviews: reviews });
+			})
+		}).catch(console.error)
+	})
+
 	// SHOW SINGLE MOVIE
 	app.get('/movies/:id', (req, res) => {
 		moviedb.movieInfo({
 			id: req.params.id
 		}).then(movie => {
-			// // **BEG SKETCHY** \\ \\
-			// Review.find({
-			// 	movieId: req.params.id
-			// }).then(reviews => {
-			// 	res.render('movies-show', {
-			// 		movie: movie,
-			// 		reviews: reviews
-			// 	});
-			// });
-			// // **END SKETCHY** \\ \\
 			if (movie.video) {
 				moviedb.movieVideos({
 					id: req.params.id

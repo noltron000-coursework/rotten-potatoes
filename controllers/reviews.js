f90e618e8e76b56151d8b96ab2a956a4174ecf51
Nolan Kovacik <noltron000@gmail.com>
2019-05-30 00:18:06 -0700

    brush-up changes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,7 +3,7 @@ const Comment = require('../models/comment');
 const MovieDB = require('moviedb-promise');
 const moviedb = new MovieDB('3a1d8db55135a8ae41b2314190591157');
 
-function reviews (app) {
+function reviews(app) {
 	// // INDEX => SHOW ALL REVIEW
 	// // COMMENTING OUT REVIEWS LANDING - SHOULD BE MOVIES LANDING
 	// app.get('/reviews', (req, res) => {
@@ -21,10 +21,10 @@ function reviews (app) {
 	// == movie route ==
 	// /movies/:id/reviews/new
 	app.get('/movies/:movieId/reviews/new', (req, res) => {
-		const movie = moviedb.movieInfo( req.params.movieId )
-		.then( movie => {
-			res.render('reviews-new', { movieId: req.params.movieId, movie: movie }) //RES render?
-		});
+		const movie = moviedb.movieInfo(req.params.movieId)
+			.then(movie => {
+				res.render('reviews-new', { movieId: req.params.movieId, movie: movie })
+			});
 	});
 
 	// SHOW SINGLE REVIEW
@@ -46,19 +46,19 @@ function reviews (app) {
 	// /movies/:id/reviews/:id
 	app.put('/reviews/:id', (req, res) => {
 		Review.findByIdAndUpdate(req.params.id, req.body)
-		.then(review => {
-			res.redirect(`/movies/${review.movieId}/reviews/${review._id}`)
-		}).catch(err => {
-			console.log(err.message)
-		})
+			.then(review => {
+				res.redirect(`/movies/${review.movieId}/reviews/${review._id}`)
+			}).catch(err => {
+				console.log(err.message)
+			})
 	})
 
 	// EDIT => SHOW EDIT FORM FOR SINGLE REVIEW
 	// == movie route ==
 	// /movies/:id/reviews/:id/edit
 	app.get('/reviews/:id/edit', function (req, res) {
-		Review.findById(req.params.id, function(err, review) {
-			res.render('reviews-edit', {review: review});
+		Review.findById(req.params.id, function (err, review) {
+			res.render('reviews-edit', { review: review });
 		})
 	})
 
@@ -67,12 +67,13 @@ function reviews (app) {
 	// /movies/:id/reviews/new
 	app.post('/movies/:movieId/reviews', (req, res) => {
 		Review.create(req.body)
-		.then((review) => {
-			console.log(review)
-			res.redirect(`/movies/${review.movieId}/reviews/${review._id}`) // Redirect to reviews/:id
-		}).catch((err) => {
-			console.log(err.message)
-		})
+			.then((review) => {
+				console.log(review)
+				res.redirect(`/movies/${review.movieId}/reviews/${review._id}`) // Redirect to reviews/:id
+			})
+			.catch((err) => {
+				console.log(err.message)
+			})
 	})
 
 

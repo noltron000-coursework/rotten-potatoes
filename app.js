9d0771e1894a18e9bcb5171bed08092ebbe0e697
Nolan Kovacik <noltron000@gmail.com>
2021-03-28 12:55:24 -0400

    [upd] app.js cleansing

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
-// INITIAL DECLARATIONS
-const Review = require('./models/review');
-const Comment = require('./models/comment');
+// Require local models & controllers
+const Review = require('./models/review')
+const Comment = require('./models/comment')
 
-const movies = require('./controllers/movies'); // initialize movies
-const reviews = require('./controllers/reviews'); // initialize reviews
+const movies = require('./controllers/movies')
+const reviews = require('./controllers/reviews')
 /*
-const admin = require('./controllers/admin'); //initialize admin
+const admin = require('./controllers/admin') //initialize admin
 const comments = require('./controllers/comments')
  */
 
-const mongoose = require('mongoose'); // once was const or var...let is used
-const express = require('express');
-const methodOverride = require('method-override');
-const bodyParser = require('body-parser'); // initialize body-parser
+// Require other npm features.
+// Here, mongoose interacts with the mongodb database.
+const mongoose = require('mongoose')
 
-const app = express(); // include express.js stuff... adding dots after app (eg app.???)!
-let exphbs = require('express-handlebars');
+// Here, express deals with various CRUD operations.
+const express = require('express')
+const exphbs = require('express-handlebars')
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/rotten-potatoes';
-const port = process.env.PORT || 3000;
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 
-// connect to mongoose
-mongoose.connect(connectionString, { useNewUrlParser: true });
+// Initialize express app.
+const app = express()
 
-// set up handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// Set up handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-// override with POST having ?_method=DELETE or ?_method=PUT
-app.use(express.static('public'));
-app.use(methodOverride('_method'));
+// Connect to mongoose.
+const connectionString = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/rotten-potatoes'
+const port = process.env.PORT ?? 3000
 
-  /*  Now we get     /
- /  to the brunt of /
-/   the project.  */
+mongoose.connect(connectionString, { useNewUrlParser: true })
+
+// Serve static files from the public folder.
+app.use(express.static('public'))
+
+// Override with operations like those with DELETE or PUT.
+app.use(methodOverride('_method'))
 
 // READY TO USE BODY-PARSER
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // ROUTES
-movies(app);
-reviews(app);
+movies(app)
+reviews(app)
 /*
-admin(app);
-comments(app);
+admin(app)
+comments(app)
 */
 
 // LISTEN
@@ -56,4 +60,4 @@ if (require.main === module) {
 	})
 }
 
-module.exports = app;
+module.exports = app

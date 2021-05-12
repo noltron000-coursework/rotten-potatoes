876dbf7ebcd3c9e3b32c340337dedb1c1cd4d6f4
Nolan Kovacik <noltron000@gmail.com>
2021-05-12 16:56:24 -0400

    [mod] enhance review model

--- models/review.js
+++ models/review.js
@@ -4,13 +4,19 @@ const express = require('express')
 const app = express()
 
 const Review = mongoose.model('Review', {
-	'title': String,
-	'content': String,
-	'rating': Number,
 	'api_movie_id': {
 		'type': String,
 		'required': true,
 	},
+	'title': String,
+	'content': String,
+	'rating': Number,
+	'author': {
+		'username': String,
+		'avatar_path': String,
+	},
+	'created_at': Date,
+	'revised_at': Date,
 })
 
 module.exports = Review
